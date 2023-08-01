4b381b4c4a2ad157a400cdcbd800c5b1d14901fb
Marie <mariehamon1724@gmail.com>
2023-08-01 17:45:43 +0200

    Page 'A propos'

---
+++ Iconic travel/Iconic travel/Iconic_travelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Iconic_travelApp.swift
+//  Iconic travel
+//
+//  Created by Marie Hamon on 01/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Iconic_travelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
