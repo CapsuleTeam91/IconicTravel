07fe8e089801a6c4a19bf548d001620717cb75e5
johanna <johannafaget@gmail.com>
2023-08-01 17:49:49 +0200

    It works

--- frontend/screens/SearchScreen.jsx
+++ frontend/screens/SearchScreen.jsx
@@ -1,22 +1,24 @@
 import { SafeAreaView, StyleSheet, Text, View, StatusBar } from 'react-native';
 import { useSelector } from 'react-redux';
-import MapView from 'react-native-maps'
+import MapView from 'react-native-maps';
 
 const SearchScreen = () => {
-	const user = useSelector((state) => state.user.value.data);
+	const user = useSelector((state) => state.user.value);
 
-	console.log(user)
+	console.log('SearchScreen', user);
 	return (
-		<SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
+		<SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
 			<View style={styles.container}>
 				<Text>Bonjour {user.firstname}</Text>
-				<MapView style={styles.map}
+				<MapView
+					style={styles.map}
 					initialRegion={{
 						latitude: user.city.latitude,
 						longitude: user.city.longitude,
 						latitudeDelta: 0.0922,
 						longitudeDelta: 0.0421,
-					}} />
+					}}
+				/>
 			</View>
 		</SafeAreaView>
 	);
@@ -26,7 +28,7 @@ const styles = StyleSheet.create({
 		flex: 1,
 		alignItems: 'center',
 		justifyContent: 'center',
-		paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+		paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
 	},
 	map: {
 		width: '100%',
