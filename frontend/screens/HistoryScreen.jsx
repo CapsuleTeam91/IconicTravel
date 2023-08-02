158f8e0e2475e44ccc11dcecdcec54e69e478024
nadia11232000 <135129710+nadia11232000@users.noreply.github.com>
2023-08-02 17:22:22 +0200

    History Commit

--- frontend/screens/HistoryScreen.jsx
+++ frontend/screens/HistoryScreen.jsx
@@ -3,23 +3,6 @@ import { STYLES_GLOBAL } from "../utils/styles";
 import React, { useEffect, useRef, useState } from "react";
 import { ERRORS, URL_EXPO } from "../utils/constants";
 const HistoryScreen = ({ navigation }) => {
-  useEffect(() => {
-    fetch(`${URL_EXPO}:3000/users`)
-      .then((response) =>
-        response.status > 400 ? response.status : response.json()
-      )
-      .then((users) => {
-        if (typeof users === "number") {
-          setError(ERRORS[`err${users}`]);
-          return;
-        }
-        if (users.result) {
-          setUsersAroundDestination(users.data);
-        } else {
-          setError(ERRORS[`err${users.status}`]);
-        }
-      });
-  }, []);
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.container}>
