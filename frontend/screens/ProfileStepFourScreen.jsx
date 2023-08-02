57132fda903030d92b4c3dbc0838478ae1296cdd
Marie <mariehamon1724@gmail.com>
2023-08-02 12:31:44 +0200

    Règles communauté

--- frontend/screens/ProfileStepFourScreen.jsx
+++ frontend/screens/ProfileStepFourScreen.jsx
@@ -1,4 +1,4 @@
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { COLORS, STYLES_GLOBAL } from '../utils/styles';
 import Button from '../components/Button';
 import ButtonIcon from '../components/ButtonIcon';
@@ -6,17 +6,20 @@ import ButtonIcon from '../components/ButtonIcon';
 const ProfileStepFourScreen = ({ navigation }) => {
 	return (
 		<SafeAreaView style={styles.container}>
-			<Text style={STYLES_GLOBAL.title}>Bienvenue</Text>
-			<Text style={[STYLES_GLOBAL.subTitle]}>
+			<Text style={[STYLES_GLOBAL.title, styles.title]}>Bienvenue</Text>
+
+			<View styles={styles.text2}>
+			<Text style={[STYLES_GLOBAL.subTitle, styles.text3]}>
 				Vous avez achevé la création de votre profil d'Iconic Traveler !
 			</Text>
-
 			<Text style={styles.welcome}>
 				Bienvenue dans notre Iconic Community. 
 				C'est un endroit qui permet de rencontrer des nouvelles personnes et de créer des liens.
 				Nous vous demandons d'être respectueux et de respecter les règles de la communauté. 
 				Au plaisir !
 			</Text>
+			</View>
+
 			<Button
 				type="primary"
 				label="Be Iconic Now !"
@@ -42,7 +45,30 @@ const styles = StyleSheet.create({
 		justifyContent: 'space-between',
 		backgroundColor: COLORS.bg,
 	},
-	
+
+	title: {
+		marginTop: 70,
+	},
+
+	text2: {
+		alignItems: 'center',
+		textAlign: 'justify',
+		marginTop: 70,
+	},
+
+	text3:  {
+		alignItems: 'center',
+		padding: 10,
+		textAlign: 'center',
+	},
+
+	welcome:{
+		alignItems: 'center',
+		textAlign: 'justify',
+		padding: 20,
+		paddingTop: 50,
+		fontSize: 17,
+	},
 });
 
 export default ProfileStepFourScreen;
