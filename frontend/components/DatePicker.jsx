f9aabc032918e622bd96d5c1f1235ae3ae2871e9
nadia11232000 <135129710+nadia11232000@users.noreply.github.com>
2023-08-04 14:10:18 +0200

    Commentaires ajouter

--- frontend/components/DatePicker.jsx
+++ frontend/components/DatePicker.jsx
@@ -1,88 +1,98 @@
-import React, { useState } from 'react';
-import { COLORS } from '../utils/styles';
-import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
-import DateTimePickerModal from 'react-native-modal-datetime-picker';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import React, { useState } from "react";
+import { COLORS } from "../utils/styles";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 const DatePicker = (props) => {
-	const [datePickerVisible, setDatePickerVisible] = useState(false);
+  // État local pour gérer la visibilité du sélecteur de date
+  const [datePickerVisible, setDatePickerVisible] = useState(false);
 
-	const showDatePicker = () => {
-		setDatePickerVisible(true);
-	};
+  // Fonction pour afficher le sélecteur de date
+  const showDatePicker = () => {
+    setDatePickerVisible(true);
+  };
 
-	const hideDatePicker = () => {
-		setDatePickerVisible(false);
-	};
+  // Fonction pour masquer le sélecteur de date
+  const hideDatePicker = () => {
+    setDatePickerVisible(false);
+  };
 
-	const styles = StyleSheet.create({
-		container: {
-			width: props.width ? props.width : '70%',
-			height: 50,
-			flexDirection: 'row',
-			alignItems: 'center',
-			justifyContent: 'space-between',
-			borderRadius: 8,
-			paddingVertical: 10,
-			paddingHorizontal: 20,
-			marginVertical: 10,
-		},
-		btnContainer: {
-			...StyleSheet.absoluteFillObject,
-			right: 20,
-			flexDirection: 'row',
-			alignItems: 'center',
-			justifyContent: 'flex-end',
-		},
-		light: {
-			container: {
-				borderWidth: 1,
-				borderColor: COLORS.darkBlue,
-				backgroundColor: COLORS.bg,
-			},
-			label: {
-				color: COLORS.darkBlue,
-			},
-		},
-		dark: {
-			container: {
-				backgroundColor: COLORS.bgDark,
-			},
-			label: {
-				color: COLORS.bg,
-			},
-		},
-	});
+  // Définition des styles du composant
+  const styles = StyleSheet.create({
+    container: {
+      width: props.width ? props.width : "70%", // Applique la largeur spécifiée dans les props ou utilise 70% par défaut
+      height: 50,
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between",
+      borderRadius: 8,
+      paddingVertical: 10,
+      paddingHorizontal: 20,
+      marginVertical: 10,
+    },
+    btnContainer: {
+      ...StyleSheet.absoluteFillObject, // Positionne le bouton de calendrier à droite du conteneur en utilisant StyleSheet.absoluteFillObject
+      right: 20,
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "flex-end",
+    },
+    light: {
+      // Styles spécifiques au thème "light"
+      container: {
+        borderWidth: 1,
+        borderColor: COLORS.darkBlue,
+        backgroundColor: COLORS.bg,
+      },
+      label: {
+        color: COLORS.darkBlue,
+      },
+    },
+    dark: {
+      // Styles spécifiques au thème "dark"
+      container: {
+        backgroundColor: COLORS.bgDark,
+      },
+      label: {
+        color: COLORS.bg,
+      },
+    },
+  });
 
-	return (
-		<View style={[styles.container, styles[props.theme].container]}>
-			<Text style={styles[props.theme].label}>{props.label}</Text>
-			<TouchableOpacity
-				onPress={showDatePicker}
-				activeOpacity={0.8}
-				style={styles.btnContainer}>
-				<Ionicons
-					name="calendar-outline"
-					size={28}
-					color={props.theme === 'dark' ? COLORS.bg : COLORS.darkBlue}
-				/>
-			</TouchableOpacity>
-			<DateTimePickerModal
-				date={props.date}
-				display="spinner"
-				isVisible={datePickerVisible}
-				mode="date"
-				onConfirm={(date) => {
-					props.onconfirm(date);
-					hideDatePicker();
-				}}
-				onCancel={hideDatePicker}
-				minimumDate={props.minimumDate}
-			/>
-		</View>
-	);
+  return (
+    <View style={[styles.container, styles[props.theme].container]}>
+      <Text style={styles[props.theme].label}>{props.label}</Text>
+      {/* Bouton pour afficher le sélecteur de date */}
+      <TouchableOpacity
+        onPress={showDatePicker}
+        activeOpacity={0.8}
+        style={styles.btnContainer}
+      >
+        <Ionicons
+          name="calendar-outline"
+          size={28}
+          color={props.theme === "dark" ? COLORS.bg : COLORS.darkBlue}
+        />
+      </TouchableOpacity>
+
+      {/* Modal pour sélectionner la date */}
+      <DateTimePickerModal
+        date={props.date}
+        display="spinner"
+        isVisible={datePickerVisible}
+        mode="date"
+        onConfirm={(date) => {
+          // Appelle la fonction onconfirm avec la date sélectionnée
+          props.onconfirm(date);
+          // Masque le sélecteur de date
+          hideDatePicker();
+        }}
+        onCancel={hideDatePicker} // Quand la date est confirmée, on appelle la fonction onconfirm avec la date en paramètre
+        minimumDate={props.minimumDate} // Date minimum autorisée pour la sélection
+      />
+    </View>
+  );
 };
-
-
 
 export default DatePicker;
