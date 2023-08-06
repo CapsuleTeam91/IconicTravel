9a1d01a86ea3bc44bed57c343b98d669b5c9119a
johanna <johannafaget@gmail.com>
2023-08-06 15:51:21 +0200

    création composant children modal langues parlées

---
+++ frontend/components/modals/UpdateLanguagesChildren.jsx
@@ -0,0 +1,54 @@
+import { StyleSheet, View } from 'react-native';
+import ButtonIcon from '../ButtonIcon';
+import DropdownLanguage from '../forms/DropdownLanguage';
+
+export const UpdateLanguagesChildren = ({
+	spokenLanguages,
+	setSpokenLanguages,
+	handleUpdate,
+	setUpdateLanguagesVisible,
+}) => {
+	return (
+		<View style={styles.inputContainer}>
+			<DropdownLanguage
+				spokenLanguages={spokenLanguages}
+				setSpokenLanguages={setSpokenLanguages}
+			/>
+
+			<View style={styles.optionsContainer}>
+				<ButtonIcon
+					type="secondary"
+					size={18}
+					name="arrow-back-outline"
+					onpress={() => {
+						setUpdateLanguagesVisible(false);
+					}}
+				/>
+				<ButtonIcon
+					type="primary"
+					size={18}
+					name="checkmark-outline"
+					onpress={() => {
+						handleUpdate({ spokenLanguages });
+						setUpdateLanguagesVisible(false);
+					}}
+				/>
+			</View>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	inputContainer: {
+		width: '100%',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	optionsContainer: {
+		width: Platform.OS === 'ios' ? '70%' : '80%',
+		flexWrap: 'wrap',
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+	},
+});
