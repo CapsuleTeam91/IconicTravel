8b4891e7fdc775ec345a55be183a0acaae390535
johanna <johannafaget@gmail.com>
2023-08-08 20:06:42 +0200

    nouvelle route pour les cities

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const API_KEY = process.env.MAP_API_KEY;
+/* GET /:city */
+router.get('/city/:city', async (req, res) => {
+	const URL = `https://www.mapquestapi.com/geocoding/v1/address?key=${API_KEY}&location=${req.params.city}`;
+	const response = await fetch(URL);
+	const items = await response.json();
+	const detailedCities = items.results[0].locations;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	const suggestions = detailedCities.map((city, i) => ({
+		id: i,
+		title: `${city.adminArea5}, ${city.adminArea4}`,
+		name: city.adminArea5,
+		latitude: city.displayLatLng.lat,
+		longitude: city.displayLatLng.lng,
+	}));
+	console.log(suggestions);
+	res.json({ result: true, suggestions });
 });
 
 module.exports = router;
