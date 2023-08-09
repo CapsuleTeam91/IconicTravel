baa8dc2717db5a5194adf722f4b9c379c0b25efe
nadia11232000 <135129710+nadia11232000@users.noreply.github.com>
2023-08-09 10:03:33 +0200

    Commentaire

--- backend/models/chatChannels.js
+++ backend/models/chatChannels.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const messageSchema = mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
-    message: String,
-    createdAt: Date
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "users" }, // Clé étrangère pour l'utilisateur lié au message
+  message: String,
+  createdAt: Date, // Date de création du message
 });
 
 const chatChannelSchema = mongoose.Schema({
-    host: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
-    traveler: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
-    name: String,
-    messages: [messageSchema],
-    createdAt: Date
+  host: { type: mongoose.Schema.Types.ObjectId, ref: "users" }, // Clé étrangère pour l'utilisateur hôte du canal
+  traveler: { type: mongoose.Schema.Types.ObjectId, ref: "users" }, // Clé étrangère pour l'utilisateur voyageur du canal
+  name: String,
+  messages: [messageSchema], // Liste des messages dans le canal
+  createdAt: Date, // Date de création du canal
 });
 
-const ChatChannel = mongoose.model('chatChannels', chatChannelSchema);
+const ChatChannel = mongoose.model("chatChannels", chatChannelSchema);
 
 module.exports = ChatChannel;
