2f5ab019a03f6a8f875236b9d956359625ec5343
johanna <johannafaget@gmail.com>
2023-08-09 12:24:36 +0200

    cleaning a bit

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,47 +1,47 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const ERRORS = {
-  required: `field required`,
-  empty: `This field cannot be empty`,
+	required: `field required`,
+	empty: `This field cannot be empty`,
 };
 
 const DEFAULT_PHOTO =
-  "https://res.cloudinary.com/dvmipcwy7/image/upload/v1690571975/ezulphnetoa65bodxzwn.jpg";
-const DEFAULT_LANGUAGE = "FR";
-const DEFAULT_HOBBY = "voyage";
-const DEFAULT_DESCRIPTION = "Je dois mettre à jour mon profil !";
+	'https://res.cloudinary.com/dvmipcwy7/image/upload/v1690571975/ezulphnetoa65bodxzwn.jpg';
+const DEFAULT_LANGUAGE = 'FR';
+const DEFAULT_HOBBY = 'voyage';
+const DEFAULT_DESCRIPTION = 'Je dois mettre à jour mon profil !';
 
 const citySchema = mongoose.Schema({
-  name: String,
-  latitude: Number,
-  longitude: Number,
+	name: String,
+	latitude: Number,
+	longitude: Number,
 });
 
 const userSchema = mongoose.Schema({
-  firstname: String,
-  lastname: String,
-  dateOfBirth: Date,
-  email: {
-    type: String,
-    required: [true, ERRORS.required],
-    lowercase: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, ERRORS.required],
-  },
-  token: String, // Token pour l'authentification de l'utilisateur
-  avatarUrl: { type: String, default: DEFAULT_PHOTO }, // URL de l'avatar de l'utilisateur
-  description: { type: String, default: DEFAULT_DESCRIPTION }, // Description par défaut
-  city: citySchema,
-  spokenLanguages: [{ type: String, default: DEFAULT_LANGUAGE }],
-  hobbies: [{ type: String, default: DEFAULT_HOBBY }],
-  bookings: [{ type: mongoose.Schema.Types.ObjectId, ref: "bookings" }],
-  chatChannels: [{ type: mongoose.Schema.Types.ObjectId, ref: "chatChannels" }],
-  canHost: { type: Boolean, default: false },
+	firstname: String,
+	lastname: String,
+	dateOfBirth: Date,
+	email: {
+		type: String,
+		required: [true, ERRORS.required],
+		lowercase: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: [true, ERRORS.required],
+	},
+	token: String,
+	avatarUrl: { type: String, default: DEFAULT_PHOTO },
+	description: { type: String, default: DEFAULT_DESCRIPTION },
+	city: citySchema,
+	spokenLanguages: [{ type: String, default: DEFAULT_LANGUAGE }],
+	hobbies: [{ type: String, default: DEFAULT_HOBBY }],
+	bookings: [{ type: mongoose.Schema.Types.ObjectId, ref: 'bookings' }],
+	chatChannels: [{ type: mongoose.Schema.Types.ObjectId, ref: 'chatChannels' }],
+	canHost: { type: Boolean, default: false },
 });
 
-const User = mongoose.model("users", userSchema);
+const User = mongoose.model('users', userSchema);
 
 module.exports = User;
--- frontend/screens/MessagesScreen.jsx
+++ frontend/screens/MessagesScreen.jsx
@@ -1,117 +1,112 @@
 import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  TouchableOpacity,
-  Image,
-  ScrollView,
-} from "react-native";
-import ButtonIcon from "../components/buttons/ButtonIcon";
-import { URL_EXPO } from "../environnement";
-import { STYLES_GLOBAL, COLORS } from "../utils/styles";
-import { useDispatch, useSelector } from "react-redux";
-import React, { useEffect, useState } from "react";
-import { useIsFocused } from "@react-navigation/native";
+	SafeAreaView,
+	StyleSheet,
+	Text,
+	View,
+	FlatList,
+	TouchableOpacity,
+	Image,
+	ScrollView,
+} from 'react-native';
+import ButtonIcon from '../components/buttons/ButtonIcon';
+import { URL_EXPO } from '../environnement';
+import { STYLES_GLOBAL, COLORS } from '../utils/styles';
+import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useIsFocused } from '@react-navigation/native';
 
 const MessagesScreen = ({ navigation }, props) => {
-  const isFocused = useIsFocused();
+	const isFocused = useIsFocused();
 
-  const user = useSelector((state) => state.user.value);
-  const [chats, setChats] = useState([]);
+	const user = useSelector((state) => state.user.value);
+	const [chats, setChats] = useState([]);
 
-  // Utilisation de useEffect pour charger les chats lorsque l'écran est focalisé
-  useEffect(() => {
-    if (isFocused) {
-      fetch(`${URL_EXPO}:3000/chats/${user.token}`) // Appel à l'API pour récupérer les chats de l'utilisateur
-        .then((resp) => resp.json())
-        .then((resp) => {
-          setChats(resp.chats); // Mise à jour de l'état local avec les chats récupérés
-        });
-    }
-  }, [isFocused]);
+	useEffect(() => {
+		if (isFocused) {
+			fetch(`${URL_EXPO}:3000/chats/${user.token}`)
+				.then((resp) => resp.json())
+				.then((resp) => {
+					setChats(resp.chats);
+				});
+		}
+	}, [isFocused]);
 
-  // Création d'une liste de chats à afficher
-  const chatList = chats.map((chat, i) => {
-    // Détermination de l'utilisateur à afficher dans la liste en fonction du rôle (traveler ou host)
-    let userToDisplay = chat.traveler;
-    if (chat.host.email !== user.email) {
-      userToDisplay = chat.host;
-    }
+	const chatList = chats.map((chat, i) => {
+		let userToDisplay = chat.traveler;
+		if (chat.host.email !== user.email) {
+			userToDisplay = chat.host;
+		}
 
-    return (
-      <View key={i} style={styles.messageContainer}>
-        <Image
-          source={{ uri: userToDisplay.avatarUrl }}
-          style={styles.avatar}
-        />
-        <Text style={styles.message}>
-          {userToDisplay.firstname} · {userToDisplay.city.name}
-        </Text>
-        <ButtonIcon
-          onpress={() => chatClicked(chat)}
-          name="arrow-forward-outline"
-          type="transparent"
-        />
-      </View>
-    );
-  });
+		return (
+			<View key={i} style={styles.messageContainer}>
+				<Image
+					source={{ uri: userToDisplay.avatarUrl }}
+					style={styles.avatar}
+				/>
+				<Text style={styles.message}>
+					{userToDisplay.firstname} · {userToDisplay.city.name}
+				</Text>
+				<ButtonIcon
+					onpress={() => chatClicked(chat)}
+					name="arrow-forward-outline"
+					type="transparent"
+				/>
+			</View>
+		);
+	});
 
-  // Gestion de l'appui sur un chat
-  const chatClicked = (chat) => {
-    navigation.navigate("Chat", { chat }); // Naviguer vers l'écran de chat en passant le chat sélectionné comme paramètre
-  };
+	const chatClicked = (chat) => {
+		navigation.navigate('Chat', { chat });
+	};
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Messages</Text>
-      <ScrollView
-        contentContainerStyle={styles.messagesContainer}
-        style={styles.messagesContainer2}
-      >
-        {chatList}
-      </ScrollView>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Messages</Text>
+			<ScrollView
+				contentContainerStyle={styles.messagesContainer}
+				style={styles.messagesContainer2}>
+				{chatList}
+			</ScrollView>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#f0f0f0",
-  },
-  title: {
-    color: COLORS.darkBlue,
-    fontSize: 20,
-    letterSpacing: 1.2,
-    fontWeight: "700",
-    textAlign: "center",
-    textTransform: "uppercase",
-    marginTop: 20,
-  },
-  messagesContainer: {
-    alignItems: "center",
-  },
-  messageContainer: {
-    marginVertical: 10,
-    padding: 10,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    width: "90%",
-    height: 50,
-    borderWidth: 1,
-    borderRadius: 20,
-  },
-  avatar: {
-    width: 42,
-    height: 42,
-    borderRadius: 50,
-  },
-  message: {
-    marginStart: 20,
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#f0f0f0',
+	},
+	title: {
+		color: COLORS.darkBlue,
+		fontSize: 20,
+		letterSpacing: 1.2,
+		fontWeight: '700',
+		textAlign: 'center',
+		textTransform: 'uppercase',
+		marginTop: 20,
+	},
+	messagesContainer: {
+		alignItems: 'center',
+	},
+	messageContainer: {
+		marginVertical: 10,
+		padding: 10,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		width: '90%',
+		height: 50,
+		borderWidth: 1,
+		borderRadius: 20,
+	},
+	avatar: {
+		width: 42,
+		height: 42,
+		borderRadius: 50,
+	},
+	message: {
+		marginStart: 20,
+	},
 });
 
 export default MessagesScreen;
